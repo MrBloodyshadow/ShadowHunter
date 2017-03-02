8e8005b3b15a55bb8158084cf8ef17516d471451
MrBloodyshadow <sergiopenalbaescudero@gmail.com>
2017-03-02 22:32:44 +0100

    Almost finished.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ShadowHunter
+
+This bot was made for a request post on https://www.reddit.com/r/RequestABot/5x2yuo.
+
+Original request:
+
+I submit a lot of spammers to /r/spam - these accounts are then reviewed by the admins and usually shadowbanned.
+Sometimes I like to look at my submission history to rediscover cool stuff but nowadays I can't do that because so much of it is just dead spam submissions.
+I would like a bot that can delete these posts from my history, or at least generate links to the submissions where the user has been deleted (so the page looks like this and the link the bot generates would look like this)
+If it could create a separate table with links to posts where the user has not been banned so I can request a re-review, that would also be helpful.
+Thanks in advance
+
+
+Account status check updated from 13steinj's comment: https://www.reddit.com/r/redditdev/comments/4sgp5y/praw_most_efficient_way_to_find_if_a_redditor_is/d59be1v/
---
+++ huting.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+import json
+import praw
+import prawcore
+import codecs
+import requests
+import urllib
+
+reddit = praw.Reddit('hunter')
+reddit.user.me()
+
+user_to_search = 'Borax'
+user_to_pm = 'SavagePewbot'
+
+
+def is_username_available(username):
+    endpoint = 'https://www.reddit.com/api/username_available.json?user='
+    url = endpoint + username
+    response = requests.get(url)
+    return response.content == 'True'
+
+
+def get_user_status(username):
+    try:
+        if getattr(reddit.redditor(username), 'is_suspended', False):
+            return 'suspended'  # account is suspended
+    except prawcore.NotFound:
+        if is_username_available(username):
+            return 'not_exists'  # account doesn't exist
+        else:
+            return 'banned'  # account is deleted or shadowbanned for spam
+    else:
+        return 'exists'  # account exists
+
+
+def get_submissions(username, limit=0, before='', sort='new'):
+    if not before.startswith('t3_'):
+        before = ''
+    endpoint = 'https://www.reddit.com/user/{}/submitted.json?sort={}&limit={}&before={}'
+    url = endpoint.format(username, sort, limit, before)
+    print(url)
+    request = urllib.request.Request(
+        url,
+        data=None,
+        headers={
+            'User-Agent': 'pls let me test the api'
+        }
+    )
+    response = urllib.request.urlopen(request)
+    # response = requests.get(url)
+    return response
+
+
+def get_spam_user(title):
+    import re
+    str_to_replace = 'Overview for '
+    start_with = re.match(str_to_replace, title, re.I)
+    if not start_with:
+        return
+
+    str_replaced = ''
+    pattern = re.compile(str_to_replace, re.IGNORECASE)
+    username = pattern.sub(str_replaced, title)
+    return username
+
+
+def get_spam_posts(username, sub_to_search='spam', limit=10):
+    response = get_submissions(username, limit=limit)
+    reader = codecs.getreader("utf-8")
+    json_dic = json.load(reader(response))
+    childrens = json_dic['data']['children']
+
+    active = []
+    banned = []
+    for children in childrens:
+        subreddit = children['data']['subreddit']
+
+        if subreddit == sub_to_search:
+            title = children['data']['title']
+
+            username = get_spam_user(title)
+            if not username:
+                continue
+            result = get_user_status(username)
+
+            id = children['data']['id']
+
+            data = []
+            data.append(id)
+            data.append(username)
+            if result == 'banned':
+                # submission = reddit.submission(id=id)
+                # submission.delete()
+                active.append(data)
+            else:
+                banned.append(data)
+    return banned, active
+
+
+def to_url(text, url):
+    return '[' + text + '](' + url + ')'
+
+
+tuples = get_spam_posts(user_to_search)
+
+banned_users = '|Banned users: \n-\n|'
+for banned in tuples[0]:
+    banned_users += to_url(banned[1], '/r/spam/' + banned[0]) + ', '
+banned_users = banned_users[:-2]
+
+separation = '\n___\n'
+
+active_users = '|Active users: \n-\n|'
+for active in tuples[1]:
+    active_users += to_url(active[1], '/r/spam/' + active[0]) + ', '
+active_users = active_users[:-2]
+
+msg = banned_users + separation + active_users
+
+reddit.redditor(user_to_pm).message('Spam report', msg)
+
+print('Done.')
